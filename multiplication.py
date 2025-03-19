565766e908df01016a3018221f24d23ccafb3261
Arbin-del <arbinshres36@gmail.com>
2025-03-19 07:49:59 +0545

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,12 @@
+def multiply(a, b):
+    return a * b
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Performing multiplication
+result = multiply(num1, num2)
+
+# Displaying the result
+print(f"The result of multiplication: {result}")
